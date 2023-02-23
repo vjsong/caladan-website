fa956c2a22f8aad2347d45abee8c46df21bee455
vjsong <vincentjtsong@gmail.com>
2023-02-22 19:41:37 -0800

    Compressed video and added oveflow-hidden

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,16 +4,16 @@ import DateTime from './DateTime'
 const Main = () => {
     return (
         <div className = 'w-full h-screen bg-black'>
-            <div className='absolute w-full h-full bg-black md:bg-black/75'></div>
-            <video  className='w-full h-full object-cover display-none md:display overflow-hidden' loop autoPlay muted>
-                <source src= '/assets/videobg_night.mp4' />
+            <div className='absolute w-full h-screen bg-black md:bg-black/75 overflow-hidden'></div>
+            <video  className='w-full h-screen object-cover display-none md:display overflow-hidden' loop autoPlay muted>
+                <source src= '/assets/videoBg.mp4' />
             </video>
             <div className=' absolute w-full h-full flex flex-col justify-center items-center text-white gap-y-6 top-0'>
                 <div className ='flex flex-col ls:flex-row justify-center items-center gap-8'>
                         <img className = 'md:w-64 w-56'src='/assets/logo_light.svg' alt="Caladan Logo"/>
                     <div className='w-auto md:w-2/5 ls:text-left text-center md:mx-0 mx-20'>
                         <p className='font-medium'>
-                            Caladan is an indpendent brokerage that sources and delivers raw materals, equipment and chemicals to climate startups
+                            Caladan is an indpendent brokerage that sources and delivers raw materals, equipment and chemicals to climate startups.
                         </p>
                     </div>
                 </div>
