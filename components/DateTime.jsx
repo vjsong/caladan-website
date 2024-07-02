30794dad9c86cc7e65637ddd2ef342f8e060b94f
Vincent Song <75090913+vjsong@users.noreply.github.com>
2024-07-02 18:32:54 -0500

    Update DateTime.jsx

--- components/DateTime.jsx
+++ components/DateTime.jsx
@@ -6,7 +6,7 @@ export const DateTime = () => {
     return(
         <div className ='flex flex-row justify-center items-center text-white gap-x-1'>
             <img className = 'h-4 w-4'src={'/assets/clock_light.svg'} alt='clock icon'/>
-            <Clock format={'HH:mm:ss'} ticking={true} timezone={'America/Belize'} blinking>
+            <Clock format={'HH:mm:ss'} ticking={true} timezone={'America/Chicago'} blinking>
             </Clock>
             <p>CST - &copy;  2023</p>
         </div>
