4e85f4264a4328386cc98e159912c116c097708b
Vincent Song <75090913+vjsong@users.noreply.github.com>
2024-07-02 18:49:12 -0500

    Update DateTime.jsx

--- components/DateTime.jsx
+++ components/DateTime.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import Clock from 'react-live-clock';
 
 export const DateTime = () => {
+    const currentYear = new Date().getFullYear();
     
-    return(
-        <div className ='flex flex-row justify-center items-center text-white gap-x-1'>
-            <img className = 'h-4 w-4'src={'/assets/clock_light.svg'} alt='clock icon'/>
-            <Clock format={'HH:mm:ss'} ticking={true} timezone={'America/Chicago'} blinking>
-            </Clock>
-            <p>CST - &copy;  2023</p>
+    return (
+        <div className='flex flex-row justify-center items-center text-white gap-x-1'>
+            <img className='h-4 w-4' src={'/assets/clock_light.svg'} alt='clock icon' />
+            <Clock format={'HH:mm:ss'} ticking={true} timezone={'America/Chicago'} />
+            <p>CST - &copy; {currentYear}</p>
         </div>
-    )
+    );
 }
 
-export default DateTime
+export default DateTime;
